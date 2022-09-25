aa4f075a0072db63fc5980f9fa05d4a14e00bfa4
Jason Watson <hi@jbw.codes>
2022-09-25 15:20:53 +0100

    add versioning to the script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 rm -rf builds
+APP_VERSION=2.0.0
+sed -i .bak 's/CURRENT_PROJECT_VERSION.*;/CURRENT_PROJECT_VERSION = '$APP_VERSION';/g' BuildingAppsOnTheCli.xcodeproj/project.pbxproj
 xcodebuild clean archive -scheme BuildingAppsOnTheCli -configuration Release -archivePath builds/archives/BuildingAppsOnTheCli.xcarchive
 xcodebuild -exportArchive -archivePath builds/archives/BuildingAppsOnTheCli.xcarchive -exportPath builds/exports/BuildingAppsOnTheCli -exportOptionsPlist BuildingAppsOnTheCli/Info.plist
