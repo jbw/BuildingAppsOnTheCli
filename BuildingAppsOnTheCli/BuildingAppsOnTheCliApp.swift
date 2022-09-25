eba8c394777bacf66fecf058dc22fb2520e34705
Jason Watson <hi@jbw.codes>
2022-09-25 12:15:57 +0100

    Initial Commit

---
+++ BuildingAppsOnTheCli/BuildingAppsOnTheCliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingAppsOnTheCliApp.swift
+//  BuildingAppsOnTheCli
+//
+//  Created by Jason Watson on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BuildingAppsOnTheCliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
